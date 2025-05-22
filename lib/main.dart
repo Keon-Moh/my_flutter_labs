1d1fc13f9eb77b5da9a61eab0588bff939c96172
Keon Moh <keon_moh@hotmail.com>
2025-05-21 22:44:01 -0400

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,73 +10,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Lab 2 Login Page',
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  var _counter = 0.0;
-  var myFontSize = 30.0;
+class _LoginPageState extends State<LoginPage> {
+  // Text controllers
+  late TextEditingController _loginController;
+  late TextEditingController _passwordController;
 
-  void setNewValue(double value) {
-    setState(() {
-      _counter = value;
-      myFontSize = value > 60.0 ? 60.0 : (value < 10.0 ? 10.0 : value);
-    });
-  }
+  // Initial image
+  String imageSource = 'images/question-mark.jpg';
 
-  void _incrementCounter() {
+  // Button click handler
+  void _handleLogin() {
+    String password = _passwordController.text;
+
     setState(() {
-      if (_counter < 100.0) {
-        _counter = (_counter + 1).clamp(0.0, 100.0);
-        myFontSize = (myFontSize + 1.0).clamp(10.0, 60.0);
+      if (password == 'QWERTY123') {
+        imageSource = 'images/light-bulb.jpg';
+      } else {
+        imageSource = 'images/stop-sign.jpg';
       }
     });
   }
 
+  @override
+  void initState() {
+    super.initState();
+    _loginController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
 
+  @override
+  void dispose() {
+    _loginController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+      appBar: AppBar(title: const Text('Login Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('You have pushed the button this many times:',
-                style:TextStyle( fontSize:myFontSize )),
-            Text('$_counter', style: TextStyle(fontSize: myFontSize)),
-            Slider(value:_counter,
-              min: 0.0,
-              max: 100.0,
-              onChanged: setNewValue,)
+          children: [
+            TextField(
+              controller: _loginController,
+              decoration: const InputDecoration(labelText: 'Login name'),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _passwordController,
+              obscureText: true, // <-- This hides password
+              decoration: const InputDecoration(labelText: 'Password'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _handleLogin,
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 20),
+            Image.asset(
+              imageSource,
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
